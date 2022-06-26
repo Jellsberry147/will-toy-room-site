84d457302a688a4b2d6f02daafa214ad45db0550
Jellsberry147 <96354179+Jellsberry147@users.noreply.github.com>
2022-06-25 22:16:32 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Will's Toy Box</header>
+      <div className="intro">
+        He'll give you the heebie-jeebies at a great price! I call that, service
+        with a smile! uwu
+      </div>
+      <div className="contact">
+        Get ahold of me at 555-555-5555... 555... 5 or check me out on the
+        socials!
+      </div>
+      <footer>
+        <p>Copyright © {new Date().getFullYear()} Smacky Inc.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
